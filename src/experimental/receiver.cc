e7e1fabdde9d7f03cfd97a667aca55a9ea0e3a8a
Danyang Zhuo <danyangzhuo@gmail.com>
2021-07-29 16:03:32 -0400

    a simple speed test

--- src/experimental/receiver.cc
+++ src/experimental/receiver.cc
@@ -7,6 +7,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <chrono>
+#include <ctime>
+#include <ratio>
+
 const uint32_t kNumMBufs = 8192;
 const uint32_t kMBufCacheSize = 256;
 
@@ -51,38 +55,56 @@ int init_port(struct rte_mempool* mbuf_pool, uint16_t port_id) {
 }
 
 int lcore_main(void* args) {
+  struct rte_mempool* mbuf_pool = static_cast<struct rte_mempool*>(args);
+
   unsigned lcore_id;
   lcore_id = rte_lcore_id();
   LOG(INFO) << "lcore_main from core " << lcore_id;
 
-  struct rte_mempool* mbuf_pool = static_cast<struct rte_mempool*>(args);
+  uint16_t total_pkts = 0;
 
   for (;;) {
+    std::chrono::high_resolution_clock::time_point start =
+        std::chrono::high_resolution_clock::now();
+
     struct rte_mbuf* pkts_burst[kMaxBurstSize];
 
-    for (auto& m : pkts_burst) {
-      m = rte_pktmbuf_alloc(mbuf_pool);
+    for (int i = 0; i < kMaxBurstSize; i++) {
+      pkts_burst[i] = rte_pktmbuf_alloc(mbuf_pool);
+      assert(pkts_burst[i]);
     };
 
-    uint16_t port_id;
+    uint16_t port_id = 0;
     // TODO(cjr): change this to stick to one port for a thread
-    RTE_ETH_FOREACH_DEV(port_id) {
-      uint16_t nb_rx = rte_eth_rx_burst(port_id, 0, pkts_burst, kMaxBurstSize);
-      if (nb_rx == 0) continue;
+    // RTE_ETH_FOREACH_DEV(port_id) {
+    uint16_t nb_rx = rte_eth_rx_burst(port_id, 0, pkts_burst, kMaxBurstSize);
+    // if (nb_rx == 0) continue;
 
-      for (size_t i = 0; i < nb_rx; i++) {
-        auto eth = rte_pktmbuf_mtod(pkts_burst[i], struct rte_ether_hdr*);
-        auto& smac = eth->s_addr.addr_bytes;
-        printf(
-            "receive a packet from %02x:%02x:%02x:%02x:%02x:%02x, length: %d\n",
-            smac[0], smac[1], smac[2], smac[3], smac[4], smac[5],
-            pkts_burst[i]->pkt_len);
-      }
-    }
+    // for (size_t i = 0; i < nb_rx; i++) {
+    //   auto eth = rte_pktmbuf_mtod(pkts_burst[i], struct rte_ether_hdr*);
+    //   auto& smac = eth->s_addr.addr_bytes;
+    //   printf(
+    //       "receive a packet from %02x:%02x:%02x:%02x:%02x:%02x, length:
+    //       %d\n", smac[0], smac[1], smac[2], smac[3], smac[4], smac[5],
+    //       pkts_burst[i]->pkt_len);
+    // }
+    // }
 
-    for (auto m : pkts_burst) {
-      rte_pktmbuf_free(m);
-    }
+    for (int i = 0; i < kMaxBurstSize; i++) {
+      rte_pktmbuf_free(pkts_burst[i]);
+    };
+
+    if (nb_rx == 0) continue;
+
+    total_pkts += nb_rx;
+
+    std::chrono::high_resolution_clock::time_point end =
+        std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_span =
+        std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+    double receiving_rate = 98 * 8 * nb_rx / time_span.count() / 1e9;
+    printf("current receving rate = %lf Gbps\n", receiving_rate);
+    printf("total pkts = %d\n", total_pkts);
   }
 
   return 0;
@@ -105,13 +127,14 @@ int main(int argc, char* argv[]) {
 
   // initialize each port
   LOG(INFO) << "initialize each port";
-  uint16_t port_id;
-  RTE_ETH_FOREACH_DEV(port_id) {
-    int err = init_port(mbuf_pool, port_id);
-    CHECK(err == 0) << "Cannot init port, err: " << err
-                    << ", port_id: " << port_id;
-  }
+  uint16_t port_id = 0;
+  // RTE_ETH_FOREACH_DEV(port_id) {
+  int err = init_port(mbuf_pool, port_id);
+  CHECK(err == 0) << "Cannot init port, err: " << err
+                  << ", port_id: " << port_id;
+  // }
 
+  // lcore_main(mbuf_pool);
   // call lcore_main() on every worker lcore
   rte_eal_mp_remote_launch(lcore_main, mbuf_pool, CALL_MAIN);
 
