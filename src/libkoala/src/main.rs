84a8caad067b3d52cf17cc587a24cfb2c58c0078
Jingrong Chen <crazyboycjr@gmail.com>
2021-10-15 16:13:42 -0400

    ipc channel established (#20)

--- src/libkoala/src/main.rs
+++ src/libkoala/src/main.rs
@@ -3,6 +3,7 @@ use libkoala::*;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     koala_register().expect("register failed");
 
-    let mask = QpAttrMask;
-    let qp_attr = query_qp(mask).unwrap();
+    // let mask = QpAttrMask;
+    // let qp_attr = query_qp(mask).unwrap();
+    Ok(())
 }
