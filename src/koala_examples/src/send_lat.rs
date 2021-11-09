f3891073ed9bf78e22c2aead5030a83a5a961a2e
Jingrong Chen <crazyboycjr@gmail.com>
2021-11-08 22:17:35 -0500

    add send_lat

---
+++ src/koala_examples/src/send_lat.rs
@@ -0,0 +1,244 @@
+use std::time;
+
+use structopt::StructOpt;
+
+use libkoala::verbs::{QpCapability, QpInitAttr, QpType, SendFlags, WcStatus};
+use libkoala::{cm, verbs};
+
+const SERVER_PORT: &str = "5000";
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(about = "Koala send/recv latency.")]
+pub struct Opts {
+    /// The address to connect, can be an IP address or domain name.
+    #[structopt(short, long)]
+    pub connect: Option<String>,
+
+    /// The port number to use.
+    #[structopt(short, long, default_value = SERVER_PORT)]
+    pub port: u16,
+
+    /// Total number of iterations.
+    #[structopt(short, long, default_value = "1000")]
+    pub total_iters: usize,
+
+    /// Number of warmup iterations.
+    #[structopt(short, long, default_value = "100")]
+    pub warm_iters: usize,
+
+    /// Message size.
+    #[structopt(short, long, default_value = "8")]
+    pub msg_size: usize,
+}
+
+fn run_server(opts: &Opts) -> Result<(), Box<dyn std::error::Error>> {
+    use libkoala::cm::{AddrFamily, AddrInfoFlags, AddrInfoHints, PortSpace};
+
+    let hints = AddrInfoHints::new(
+        AddrInfoFlags::PASSIVE,
+        Some(AddrFamily::Inet),
+        QpType::RC,
+        PortSpace::TCP,
+    );
+
+    let ai =
+        cm::getaddrinfo(None, Some(&opts.port.to_string()), Some(&hints)).expect("getaddrinfo");
+
+    eprintln!("ai: {:?}", ai);
+
+    let qp_init_attr = QpInitAttr {
+        qp_context: Some(&3),
+        send_cq: None,
+        recv_cq: None,
+        srq: None,
+        cap: QpCapability {
+            max_send_wr: (opts.warm_iters + opts.total_iters) as _,
+            max_recv_wr: (opts.warm_iters + opts.total_iters) as _,
+            max_send_sge: 1,
+            max_recv_sge: 1,
+            max_inline_data: 128,
+        },
+        qp_type: QpType::RC,
+        sq_sig_all: false,
+    };
+
+    let listen_id = cm::CmId::create_ep(&ai, None, Some(&qp_init_attr))?;
+
+    eprintln!("listen_id created");
+
+    listen_id.listen(16)?;
+    let id = listen_id.get_requst()?;
+
+    let mut recv_msg = vec![0u8; opts.msg_size];
+    let recv_mr = id.reg_msgs(&recv_msg)?;
+
+    let send_msg = vec![42u8; opts.msg_size];
+    let send_mr = id.reg_msgs(&send_msg)?;
+
+    for _ in 0..opts.warm_iters + opts.total_iters {
+        unsafe {
+            id.post_recv(0, &mut recv_msg, &recv_mr)?;
+        }
+    }
+
+    id.accept(None)?;
+    eprintln!("accepted a connection");
+
+    // busy poll
+    let mut wc = Vec::with_capacity(32);
+    let cq = &id.qp.as_ref().unwrap().recv_cq;
+    let mut cqe = 0;
+    while cqe < opts.warm_iters + opts.total_iters {
+        cq.poll_cq(&mut wc)?;
+        for c in &wc {
+            assert_eq!(c.status, WcStatus::Success);
+            cqe += 1;
+            let send_flags = if cqe == opts.warm_iters + opts.total_iters {
+                SendFlags::SIGNALED
+            } else {
+                Default::default()
+            };
+            id.post_send(0, &send_msg, &send_mr, send_flags)?;
+        }
+    }
+
+    let send_wc = id.get_send_comp()?;
+    assert_eq!(send_wc.status, WcStatus::Success);
+
+    println!("{:?}", &recv_msg[..100.min(opts.msg_size)]);
+
+    assert_eq!(&recv_msg, &vec![42u8; opts.msg_size],);
+    Ok(())
+}
+
+fn run_client(opts: &Opts) -> Result<(), Box<dyn std::error::Error>> {
+    let ai = cm::getaddrinfo(
+        Some(&opts.connect.as_deref().unwrap()),
+        Some(&opts.port.to_string()),
+        None,
+    )?;
+
+    eprintln!("ai: {:?}", ai);
+
+    let qp_init_attr = QpInitAttr {
+        qp_context: Some(&3),
+        send_cq: None,
+        recv_cq: None,
+        srq: None,
+        cap: QpCapability {
+            max_send_wr: (opts.warm_iters + opts.total_iters) as _,
+            max_recv_wr: (opts.warm_iters + opts.total_iters) as _,
+            max_send_sge: 1,
+            max_recv_sge: 1,
+            max_inline_data: 128,
+        },
+        qp_type: QpType::RC,
+        sq_sig_all: false,
+    };
+
+    let id = cm::CmId::create_ep(&ai, None, Some(&qp_init_attr))?;
+
+    eprintln!("cmid created");
+
+    id.connect(None)?;
+    eprintln!("connected to remote side");
+
+    // post receives in advance
+    let mut recv_msg = vec![0u8; opts.msg_size];
+    let recv_mr = id.reg_msgs(&recv_msg)?;
+
+    for _ in 0..opts.warm_iters + opts.total_iters {
+        unsafe {
+            id.post_recv(0, &mut recv_msg, &recv_mr)?;
+        }
+    }
+
+    let no_signal = Default::default();
+    let send_msg = vec![42u8; opts.msg_size];
+    let send_mr = id.reg_msgs(&send_msg)?;
+
+    // start sending
+    id.post_send(0, &send_msg, &send_mr, no_signal)?;
+
+    let start_ts = time::Instant::now();
+    let mut ts = time::Instant::now();
+    let mut stats = Vec::with_capacity(opts.total_iters);
+
+    let mut wc = Vec::with_capacity(1);
+    let cq = &id.qp.as_ref().unwrap().recv_cq;
+    let mut cqe = 0;
+    while cqe < opts.total_iters + opts.warm_iters {
+        cq.poll_cq(&mut wc)?;
+        cqe += wc.len();
+        for c in &wc {
+            assert_eq!(c.status, WcStatus::Success, "{:?}", c);
+
+            // record the latency from post_send to receive completion is polled
+            if cqe > opts.warm_iters {
+                stats.push(ts.elapsed());
+            }
+
+            if cqe >= opts.warm_iters {
+                // start timing
+                ts = time::Instant::now();
+            }
+
+            let send_flags = if cqe == opts.warm_iters + opts.total_iters {
+                SendFlags::SIGNALED
+            } else {
+                no_signal
+            };
+            id.post_send(0, &send_msg, &send_mr, send_flags)?;
+        }
+    }
+
+    let send_wc = id.get_send_comp()?;
+    assert_eq!(send_wc.status, WcStatus::Success);
+
+    let dura = start_ts.elapsed();
+    eprintln!(
+        "duration: {:?}, avg: {:?}, median: {:?}, min: {:?}, max: {:?}",
+        dura,
+        get_avg(&stats),
+        get_median(&stats),
+        get_min(&stats),
+        get_max(&stats),
+    );
+
+    Ok(())
+}
+
+fn get_avg(stats: &[time::Duration]) -> time::Duration {
+    let s: time::Duration = stats.iter().sum();
+    s / stats.len() as u32
+}
+
+fn get_median(stats: &[time::Duration]) -> time::Duration {
+    let mut v = stats.to_owned();
+    v.sort();
+    assert!(!v.is_empty());
+    v[v.len() / 2]
+}
+
+fn get_min(stats: &[time::Duration]) -> time::Duration {
+    let mut v = stats.to_owned();
+    v.sort();
+    v[0]
+}
+
+fn get_max(stats: &[time::Duration]) -> time::Duration {
+    let mut v = stats.to_owned();
+    v.sort();
+    assert!(!v.is_empty());
+    *v.last().unwrap()
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    if opts.connect.is_some() {
+        run_client(&opts).unwrap();
+    } else {
+        run_server(&opts).unwrap();
+    }
+}
