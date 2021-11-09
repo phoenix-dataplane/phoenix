b02f1537bb50770b9ee9f124ec7eb0567b3a5233
Jingrong Chen <crazyboycjr@gmail.com>
2021-11-08 21:13:23 -0500

    add koala_send_bw

--- src/koala_examples/src/send_bw.rs
+++ src/koala_examples/src/send_bw.rs
@@ -1,29 +1,129 @@
-use libkoala::verbs::{SendFlags, WcStatus};
-use libkoala::{cm, verbs};
 use std::time;
 
-const SERVER_ADDR: &str = "192.168.211.194";
-const SERVER_PORT: u16 = 5000;
+use structopt::StructOpt;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ai = cm::getaddrinfo(Some(&SERVER_ADDR), Some(&SERVER_PORT.to_string()), None)
-        .expect("getaddrinfo");
+use libkoala::verbs::{QpCapability, QpInitAttr, QpType, SendFlags, WcStatus};
+use libkoala::{cm, verbs};
+
+const SERVER_PORT: &str = "5000";
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(about = "Koala send/recv bandwidth")]
+pub struct Opts {
+    /// The address to connect, can be an IP address or domain name.
+    #[structopt(short, long)]
+    pub connect: Option<String>,
+
+    /// The port number to use.
+    #[structopt(short, long, default_value = SERVER_PORT)]
+    pub port: u16,
+
+    /// Total number of iterations.
+    #[structopt(short, long, default_value = "1000")]
+    pub total_iters: usize,
+
+    /// Number of warmup iterations.
+    #[structopt(short, long, default_value = "100")]
+    pub warm_iters: usize,
+
+    /// Message size.
+    #[structopt(short, long, default_value = "65536")]
+    pub msg_size: usize,
+}
+
+fn run_server(opts: &Opts) -> Result<(), Box<dyn std::error::Error>> {
+    use libkoala::cm::{AddrFamily, AddrInfoFlags, AddrInfoHints, PortSpace};
+
+    let hints = AddrInfoHints::new(
+        AddrInfoFlags::PASSIVE,
+        Some(AddrFamily::Inet),
+        QpType::RC,
+        PortSpace::TCP,
+    );
+
+    let ai =
+        cm::getaddrinfo(None, Some(&opts.port.to_string()), Some(&hints)).expect("getaddrinfo");
 
     eprintln!("ai: {:?}", ai);
 
-    let qp_init_attr = verbs::QpInitAttr {
+    let qp_init_attr = QpInitAttr {
         qp_context: Some(&3),
         send_cq: None,
         recv_cq: None,
         srq: None,
-        cap: verbs::QpCapability {
-            max_send_wr: 1024,
-            max_recv_wr: 128,
+        cap: QpCapability {
+            max_send_wr: 1,
+            max_recv_wr: (opts.warm_iters + opts.total_iters) as _,
             max_send_sge: 1,
             max_recv_sge: 1,
             max_inline_data: 128,
         },
-        qp_type: verbs::QpType::RC,
+        qp_type: QpType::RC,
+        sq_sig_all: false,
+    };
+
+    let listen_id = cm::CmId::create_ep(&ai, None, Some(&qp_init_attr))?;
+
+    eprintln!("listen_id created");
+
+    listen_id.listen(16)?;
+    let id = listen_id.get_requst()?;
+
+    let mut recv_msg = vec![0u8; 65536];
+    let recv_mr = id.reg_msgs(&recv_msg)?;
+
+    for _ in 0..opts.warm_iters + opts.total_iters {
+        unsafe {
+            id.post_recv(0, &mut recv_msg, &recv_mr)?;
+        }
+    }
+
+    id.accept(None)?;
+    eprintln!("accepted a connection");
+
+    // busy poll
+    let mut wc = Vec::with_capacity(32);
+    let cq = &id.qp.as_ref().unwrap().recv_cq;
+    let mut cqe = 0;
+    while cqe < opts.warm_iters + opts.total_iters {
+        cq.poll_cq(&mut wc)?;
+        if !wc.is_empty() {
+            break;
+        }
+        cqe += wc.len();
+        for c in &wc {
+            assert_eq!(c.status, WcStatus::Success);
+        }
+    }
+
+    println!("{:?}", &recv_msg[..100]);
+
+    assert_eq!(&recv_msg, &vec![42u8; 65536],);
+    Ok(())
+}
+
+fn run_client(opts: &Opts) -> Result<(), Box<dyn std::error::Error>> {
+    let ai = cm::getaddrinfo(
+        Some(&opts.connect.as_deref().unwrap()),
+        Some(&opts.port.to_string()),
+        None,
+    )?;
+
+    eprintln!("ai: {:?}", ai);
+
+    let qp_init_attr = QpInitAttr {
+        qp_context: Some(&3),
+        send_cq: None,
+        recv_cq: None,
+        srq: None,
+        cap: QpCapability {
+            max_send_wr: (opts.warm_iters + opts.total_iters) as _,
+            max_recv_wr: 1,
+            max_send_sge: 1,
+            max_recv_sge: 1,
+            max_inline_data: 128,
+        },
+        qp_type: QpType::RC,
         sq_sig_all: false,
     };
 
@@ -31,35 +131,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     eprintln!("cmid created");
 
-    id.connect(None).expect("Connect failed!");
-
-    let ts = time::Instant::now();
+    id.connect(None)?;
+    eprintln!("connected to remote side");
 
     let send_flags = SendFlags::SIGNALED;
     let send_msg = vec![42u8; 65536];
-    let send_mr = id.reg_msgs(&send_msg).expect("Memory registration failed!");
-    for _ in 0..1000 {
-        id.post_send(0, &send_msg, &send_mr, send_flags)
-            .expect("Post send failed!");
+    let send_mr = id.reg_msgs(&send_msg)?;
+    for _ in 0..opts.warm_iters {
+        id.post_send(0, &send_msg, &send_mr, send_flags)?;
+    }
+
+    let mut wc = Vec::with_capacity(32);
+    let cq = &id.qp.as_ref().unwrap().send_cq;
+    let mut cqe = 0;
+    while cqe < opts.warm_iters {
+        cq.poll_cq(&mut wc)?;
+        cqe += wc.len();
+        for c in &wc {
+            assert_eq!(c.status, WcStatus::Success, "{:?}", c);
+        }
+    }
+
+    let ts = time::Instant::now();
+
+    for _ in 0..opts.total_iters {
+        id.post_send(0, &send_msg, &send_mr, send_flags)?;
     }
 
     let dura = ts.elapsed();
     eprintln!("post_send finished, duration: {:?}", dura,);
 
     // busy poll
-    let mut wc = Vec::with_capacity(32);
-    let cq = id.qp.unwrap().send_cq;
     let mut cqe = 0;
-    while cqe < 1000 {
-        loop {
-            cq.poll_cq(&mut wc).expect("poll_cq failed");
-            if !wc.is_empty() {
-                break;
-            }
-        }
+    while cqe < opts.total_iters {
+        cq.poll_cq(&mut wc)?;
         cqe += wc.len();
         for c in &wc {
-            assert_eq!(c.status, WcStatus::Success);
+            assert_eq!(c.status, WcStatus::Success, "{:?}", c);
         }
     }
 
@@ -71,4 +179,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     Ok(())
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    if opts.connect.is_some() {
+        run_client(&opts).unwrap();
+    } else {
+        run_server(&opts).unwrap();
+    }
 }
