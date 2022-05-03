5ce2bdd627fcc834dfaff3fb620d3d6bd9ca3c93
Jingrong Chen <crazyboycjr@gmail.com>
2022-05-03 15:41:50 -0400

    minor change

--- scripts/count_loc.sh
+++ scripts/count_loc.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+function info {
+	echo "$(tput bold)$(tput setaf 10)"$*"$(tput sgr 0)"
+}
 
-echo "miscellaneous code:"
+
+info "miscellaneous code:"
 find src/experimental -type f -name "*.rs" | xargs wc -l
-echo "rdma bindings:"
-find src/rdma -type f -name "*.rs" | xargs wc -l
-echo "koala examples:"
+info "rdma bindings:"
+find src/rdma -type f -name "*.rs" | grep -v bindings | xargs wc -l
+info "koala examples:"
 find src/koala_examples -type f -name "*.rs" | xargs wc -l
 
-echo "koala total:"
+info "koala total:"
 find src/ -type f -name "*.rs" | grep -v "experimental" | grep -v bindings.rs | grep -v koala_examples | xargs wc -l
