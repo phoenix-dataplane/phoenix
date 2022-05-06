70d5117dc673aa8a1d95709f409c84c5bb3beca3
Jingrong Chen <crazyboycjr@gmail.com>
2022-05-05 22:57:26 -0400

    update count_loc.sh

--- scripts/count_loc.sh
+++ scripts/count_loc.sh
@@ -7,10 +7,12 @@ function info {
 
 info "miscellaneous code:"
 find src/experimental -type f -name "*.rs" | xargs wc -l
+info "3rdparty slabmalloc:"
+find src/slabmalloc -type f -name "*.rs" | xargs wc -l
 info "rdma bindings:"
 find src/rdma -type f -name "*.rs" | grep -v bindings | xargs wc -l
 info "koala examples:"
 find src/koala_examples -type f -name "*.rs" | xargs wc -l
 
 info "koala total:"
-find src/ -type f -name "*.rs" | grep -v "experimental" | grep -v bindings.rs | grep -v koala_examples | xargs wc -l
+find src/ -type f -name "*.rs" | grep -v "experimental" | grep -v slabmalloc | grep -v bindings.rs | grep -v koala_examples | xargs wc -l
