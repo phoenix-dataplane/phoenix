30bfc8cff5b4b1e6af1a995a3faa306c1eae5b25
Jingrong Chen <crazyboycjr@gmail.com>
2022-05-12 20:57:00 -0400

    minor change

--- src/koala/src/engine/runtime.rs
+++ src/koala/src/engine/runtime.rs
@@ -12,7 +12,7 @@ use super::{EngineContainer, EngineLocalStorage, EngineResult};
 
 thread_local! {
     /// To emulate a thread local storage (TLS). This should be called engine-local-storage (ELS).
-    pub static ENGINE_LS: RefCell<Option<&'static dyn EngineLocalStorage>> = RefCell::new(None);
+    pub(crate) static ENGINE_LS: RefCell<Option<&'static dyn EngineLocalStorage>> = RefCell::new(None);
 }
 
 /// This indicates the runtime of an engine's status.
