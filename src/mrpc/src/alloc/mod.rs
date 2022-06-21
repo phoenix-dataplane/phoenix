72833deaefcb0f4c0632ce7df4646ab6d6e40547
libertyeagle <wuyongji317@gmail.com>
2022-06-20 23:52:00 -0400

    fmt

--- src/mrpc/src/alloc/mod.rs
+++ src/mrpc/src/alloc/mod.rs
@@ -7,7 +7,7 @@ mod raw_vec;
 #[allow(unused)]
 mod vec;
 // SAFETY: It is the caller's responsibility to ensure all alloc's types are created on sender heap
-// otherwise a ManuallyDrop wrapper is required 
+// otherwise a ManuallyDrop wrapper is required
 // to prevent improper deallocation
 // as in `Drop` impl, we consider memory is allocated on sender heap.
 pub(crate) use boxed::Box;
