b5ceac1063558109cb19fee826d7b5e93182d0bc
libertyeagle <30348494+libertyeagle@users.noreply.github.com>
2022-06-21 02:47:59 -0400

    Update src/koala/src/mrpc/shadow/mod.rs
    Co-authored-by: Jingrong Chen <crazyboycjr@gmail.com>

--- src/koala/src/mrpc/shadow/mod.rs
+++ src/koala/src/mrpc/shadow/mod.rs
@@ -1,2 +1,6 @@
+//! Shadow data types have identical memory layouts with mRPC data types in the
+//! userland library, but they expose the details for marshaling and only
+//! implement minimal necessary functions.
+
 pub(crate) mod raw_vec;
 pub(crate) mod vec;
