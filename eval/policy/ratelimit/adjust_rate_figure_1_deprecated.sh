28255224d786ce68478a8ff4875a2ebbfc40e021
Jingrong Chen <crazyboycjr@gmail.com>
2022-08-22 18:51:44 -0400

    add scripts for mico-bench bandwidth

--- eval/policy/ratelimit/adjust_rate_figure_1_deprecated.sh
+++ eval/policy/ratelimit/adjust_rate_figure_1_deprecated.sh
@@ -3,7 +3,16 @@
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
 
 rates=(
-10000000
+1000
+# 50000
+100000
+# 200000
+# 350000
+300000
+500000
+700000
+900000
+1100000
 )
 
 for rate in ${rates[@]}; do
--- eval/policy/ratelimit/start_traffic.sh
+++ eval/policy/ratelimit/start_traffic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # start the traffic
-cargo rr --bin launcher -- -o /tmp/output --benchmark ./rpc_bench_tput_32b.toml --configfile ./config.toml
+cargo rr --bin launcher -- -o /tmp/mrpc-eval --benchmark ./rpc_bench_tput_32b.toml --configfile ./config.toml
