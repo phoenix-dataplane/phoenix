c282ac766e4a2147edb1a2df3f55af3b17042b60
Jingrong Chen <crazyboycjr@gmail.com>
2022-08-25 16:28:12 -0400

    update deploy_plugins.sh

--- scripts/deploy_plugins.sh
+++ scripts/deploy_plugins.sh
@@ -1,7 +1,8 @@
 #/usr/bin/env bash
 
 rm -rf /tmp/koala/plugins
-mkdir /tmp/koala/plugins
+mkdir -p /tmp/koala/plugins
+
 install -Dm755 target/release/libkoala_mrpc_plugin.so /tmp/koala/plugins/libkoala_mrpc_plugin.so
 install -Dm755 target/release/libkoala_rpc_adapter_plugin.so /tmp/koala/plugins/libkoala_rpc_adapter_plugin.so
 install -Dm755 target/release/libkoala_transport_rdma_plugin.so /tmp/koala/plugins/libkoala_transport_rdma_plugin.so
