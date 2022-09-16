fb90b27a5b4fd5dd5e3b3992eedada454175052a
lsh <lshzy137@gmail.com>
2022-09-16 12:35:24 -0400

    update rdma eval

--- eval/micro-bench/rate/start_traffic_tcp.sh
+++ eval/micro-bench/rate/start_traffic_tcp.sh
@@ -13,8 +13,8 @@ cd $workdir
 
 concurrency=128
 sed -i 's/--concurrency [0-9]*/--concurrency '"$concurrency"'/g' ../../../benchmark/benchmark/rpc_bench_rate/*.toml
-sed -i 's/-D [0-9]*/-D 15/g' ../../../benchmark/benchmark/rpc_bench_rate/*.toml
-sed -i 's/timeout_secs = [0-9]*/timeout_secs = 20/g' ../../../benchmark/benchmark/rpc_bench_tput/*.toml
+sed -i 's/-D [0-9]*/-D 10/g' ../../../benchmark/benchmark/rpc_bench_rate/*.toml
+sed -i 's/timeout_secs = [0-9]*/timeout_secs = 15/g' ../../../benchmark/benchmark/rpc_bench_tput/*.toml
 sed -i 's/transport =\(.*\)/transport = "Tcp"/g' koala.toml
 timestamp=$(date +%s%N)
 
