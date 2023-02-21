4104f6eff97918c698a780a31ea14d46cc552bd3
Eric Chai <echai58@gmail.com>
2023-02-21 17:32:53 -0500

    working allgather benchmark

--- src/phoenix_examples/allgather/src/main.rs
+++ src/phoenix_examples/allgather/src/main.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::thread;
 use std::time;
@@ -8,7 +7,6 @@ use structopt::StructOpt;
 use libphoenix::cm;
 use libphoenix::verbs::{MemoryRegion, WcStatus, SendFlags};
 
-const DEFAULT_PORT: &str = "6000";
 
 #[derive(StructOpt, Debug, Clone)]
 #[structopt(about = "Allgather benchmark.")]
@@ -18,7 +16,7 @@ pub struct Opts {
     pub hosts: String,
 
     /// The port number to use if not given.
-    #[structopt(short, long, default_value = DEFAULT_PORT)]
+    #[structopt(short, long, default_value = "5000")]
     pub port: u16,
 
     /// Total number of iterations.
@@ -30,7 +28,7 @@ pub struct Opts {
     pub warm_iters: usize,
 
     /// Message size.
-    #[structopt(short, long, default_value = "10")]
+    #[structopt(short, long, default_value = "1024000")]
     pub msg_size: usize,
 }
 
@@ -65,10 +63,9 @@ fn get_host_list(opts: &Opts) -> Vec<SocketAddr> {
 #[derive(Debug)]
 struct Communicator {
     send_id: cm::CmId,
-    recv_id: cm::CmId,
     send_mr: MemoryRegion<u8>,
+    recv_id: cm::CmId,
     recv_mrs: Vec<MemoryRegion<u8>>,
-    rank: usize,
 }
 
 impl Communicator {
@@ -82,83 +79,73 @@ impl Communicator {
             .expect("This host is not included in the host list");
 
         let left_peer_addr = &hosts[(rank + hosts.len() - 1) % hosts.len()];
+
+        // connect in a ring setup
         let (send_id, recv_id, recv_mrs) = {
-            if rank == hosts.len() - 1 {
-                let (recv_id, recv_mrs) = Communicator::connect(left_peer_addr, opts.msg_size, hosts.len() - 1)?;
-                let send_id = Communicator::accept(listen_addr, opts.msg_size)?;
+            // if it is the last host, then connect to left peer first and accept from right peer
+            // if it is any other host, accept first, then connect after
+            // this avoids a deadlock situation
+            if rank == hosts.len() - 1 { 
+                let (recv_id, recv_mrs) = Communicator::connect(left_peer_addr, opts.msg_size)?;
+                let send_id = Communicator::accept(listen_addr)?;
                 (send_id,recv_id,recv_mrs)
             } else{
-                let send_id = Communicator::accept(listen_addr, opts.msg_size)?;
-                let (recv_id, recv_mrs) = Communicator::connect(left_peer_addr, opts.msg_size, hosts.len() - 1)?;
+                let send_id = Communicator::accept(listen_addr)?;
+                let (recv_id, recv_mrs) = Communicator::connect(left_peer_addr, opts.msg_size)?;
                 (send_id,recv_id,recv_mrs)
             }
         };
-
-
-        //send_mr for node with rank + 1
         let send_mr = {
             let mut mr = send_id.alloc_msgs(opts.msg_size)?;
-            mr.fill((rank + 1) as u8);
+            mr.fill(42u8);
             mr
         };
 
         Ok(Communicator {
             send_id,
-            recv_id,
             send_mr,
+            recv_id,
             recv_mrs,
-            rank
         })
     }
 
-    fn accept (listen_addr:&SocketAddr, msg_size:usize) ->  Result<cm::CmId, libphoenix::Error>{
+    fn accept (listen_addr:&SocketAddr) ->  Result<cm::CmId, libphoenix::Error>{
         // create a listener
         let listener = cm::CmIdListener::bind(listen_addr)?;
 
-        //accept node with rank + 1
-        println!("Socket is listening for connection");
+        // accept node with rank + 1
         let mut builder = listener.get_request()?;
         let pre_id = builder.set_max_send_wr(2).set_max_recv_wr(2).build()?;
 
-        // allocate messages before accepting            
-        // allocate messages before connecting
-        let mut recv_mr: MemoryRegion<u8> = pre_id.alloc_msgs(msg_size)?;
-        for _ in 0..2 {
-            unsafe {
-                pre_id.post_recv(&mut recv_mr, .., 0)?;
-            }
-        }
-        //socket accepts connections
-        println!("Socket accepted connection");
+        // socket accepts connections
         pre_id.accept(None)
     }
 
-    fn connect(peer_addr:&SocketAddr, msg_size:usize, num_peers:usize)->Result<(cm::CmId,Vec<MemoryRegion<u8>>), libphoenix::Error> {
-        //connect to node with rank - 1
+    fn connect(peer_addr:&SocketAddr, msg_size:usize)->Result<(cm::CmId,Vec<MemoryRegion<u8>>), libphoenix::Error> {
+        // connect to node with rank - 1
         let mut retry = 0;
-        println!("Attempting connection to {:?}",peer_addr);
         let (recv_id,recv_mrs) = loop {
             let builder = cm::CmIdBuilder::new()
                 .set_max_recv_wr(2)
                 .set_max_recv_wr(2)
                 .resolve_route(peer_addr)?;
             let pre_id = builder.build()?;
-            let mut recv_mrs = Vec::with_capacity(num_peers);
-            // allocate messages before connecting
-            for _ in 0..num_peers{
-                let mut recv_mr: MemoryRegion<u8> = pre_id.alloc_msgs(msg_size)?;
-                for _ in 0..2 {
-                    unsafe {
-                        pre_id.post_recv(&mut recv_mr, .., 0)?;
-                    }
-                }
-                recv_mrs.push(recv_mr);
-            }
             
+            // wrap in a vec to fix a borrowing compiler error
+            let mut recv_mrs = Vec::with_capacity(1);
 
+            // allocate message before connecting
+            let mut recv_mr: MemoryRegion<u8> = pre_id.alloc_msgs(msg_size)?;
+            for _ in 0..2 {
+                unsafe {
+                    pre_id.post_recv(&mut recv_mr, .., 0)?;
+                }
+            }
+            recv_mrs.push(recv_mr);
+            
+            
             match pre_id.connect(None) {
                 Ok(id) => {
-                    println!("Successful connection");
                     break (id,recv_mrs)
                 }
                 Err(libphoenix::Error::Connect(e)) => {
@@ -177,54 +164,52 @@ impl Communicator {
 }
 
 fn run_single_thread (opts: &Opts, mut comm: Communicator) -> anyhow::Result<()> {
+    let mut start = time::Instant::now();
     let num_hosts = get_host_list(opts).len();
-    
-    for i in 0..(num_hosts - 1){
-        let recv_rank = (comm.rank + num_hosts - 1) % num_hosts;
 
-        println!("{}",i);
+    for iter in 0..opts.warm_iters + opts.total_iters {
+        if iter == opts.warm_iters {
+            start = time::Instant::now();
+        }
 
-        let send_mr = if i == 0 {
-                let mut mr = comm.send_id.alloc_msgs(opts.msg_size)?;
-                mr.copy_from_slice(comm.send_mr.as_slice());
-                mr
+        // send/receive n-1 times to distribute all information to all nodes
+        for i in 0..(num_hosts - 1){
+            unsafe{
+                comm.send_id.post_send(&comm.send_mr, .., i as u64, SendFlags::SIGNALED)?;
             }
-            else {
-            let mut mr = comm.send_id.alloc_msgs(opts.msg_size)?;
-            mr.copy_from_slice(comm.recv_mrs[i-1].as_slice());
-            mr
-        };
-        println!("send data:  {:?}", send_mr.as_slice());
-        println!("send mr made");
-        unsafe{
-            comm.send_id.post_send(&send_mr, .., i as u64, SendFlags::SIGNALED)?;
-        }
-        println!("post send");
+            let wc = comm.send_id.get_send_comp()?;
+            assert_eq!(wc.status, WcStatus::Success, "wc: {:?}", wc);
+            let wc = comm.recv_id.get_recv_comp()?;
+            assert_eq!(wc.status, WcStatus::Success, "wc: {:?}", wc);
 
-        println!("get send comp");
-        let wc = comm.send_id.get_send_comp()?;
-        assert_eq!(wc.status, WcStatus::Success, "wc: {:?}", wc);
-        println!("get recv comp");
-        let wc = comm.recv_id.get_recv_comp()?;
-        assert_eq!(wc.status, WcStatus::Success, "wc: {:?}", wc);
+            unsafe {
+                comm.recv_id.post_recv(&mut comm.recv_mrs[0], .., i as u64)?;
+            }
 
-        println!("push new data");
-        unsafe {
-            comm.recv_id.post_recv(&mut comm.recv_mrs[i], .., i as u64)?;
+            debug_assert_eq!(
+                comm.recv_mrs[0].as_slice(),
+                &vec![42u8; opts.msg_size],
+            );
         }
-        println!("recv mr: {:?}",comm.recv_mrs[i].as_slice());
-        // debug_assert_eq!(
-        //     comm.recv_mrs[i].as_slice(),
-        //     &vec![(recv_rank + 1) as u8; opts.msg_size],
-        // );
-        }
+    }
+
+    let dura = start.elapsed();
+    println!(
+        "allgather, {} workers, duration: {:?}, bandwidth: {} Gb/s",
+        num_hosts,
+        dura,
+        8e-9 * opts.total_iters as f64
+            * opts.msg_size as f64
+            * (num_hosts - 1) as f64
+            / dura.as_secs_f64(),
+    );
 
     Ok(())
 }
 
-
+// cargo run --bin allgather -- --hosts rdma0.danyang-06,rdma0.danyang-05,rdma0.danyang-04
 fn main() {
     let opts = Opts::from_args();
     let comm = Communicator::new(&opts).expect("Create communicator failed");
-    run_single_thread(&opts,comm);
+    run_single_thread(&opts,comm).unwrap()
 }
