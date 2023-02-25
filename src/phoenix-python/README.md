aa6c3a94594ddb8120b96853c0be64300d276b0f
Eric Chai <echai58@gmail.com>
2023-02-25 14:03:33 -0500

    python args for sallc/mrpc services

---
+++ src/phoenix-python/README.md
@@ -0,0 +1,5 @@
+## To run:
+
+Need to enable python virtual environment: `pyenv activate`
+
+To build the python plugin: `maturin develop`
--- src/phoenix-python/phoenix_python/test.py
+++ src/phoenix-python/phoenix_python/test.py
@@ -1,9 +1,20 @@
 from phoenix_python import shmservice
+import argparse
 
 def main():
-    a = shmservice.Hint(shmservice.Mode.Dedicate)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", required = True)
+    argument = parser.parse_args()
+    service = argument.s
 
-    shmservice.shm_register("/tmp/phoenix","control.sock","Salloc",a)
+    if service == "salloc":
+        hint = shmservice.Hint(shmservice.Mode.Dedicate)
+        shmservice.shm_register("/tmp/phoenix","control.sock","Salloc",hint)
+    elif service == "mrpc-tcp":
+        hint = shmservice.Hint(shmservice.Mode.Dedicate)
+        shmservice.shm_register("/tmp/phoenix","control.sock","Mrpc",hint,"{\"transport\":\"Tcp\",\"nic_index\":0,\"core_id\":null}")
+    else:
+        raise RuntimeError("Invalid service type requested")
 
 if __name__ == "__main__":
     main()
