77836f1caf3c292a1bb3d9ba49e86aae12c1d787
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-03-21 15:31:02 -0700

    Add extensive comments to rpc_hello example (#226)
    * add comments for rpc hello
    
    * fix formatting
    
    * tweaks

--- experimental/mrpc/examples/rpc_hello/src/server.rs
+++ experimental/mrpc/examples/rpc_hello/src/server.rs
@@ -1,7 +1,10 @@
+//! This code defines a simple mRPC server that implements the Greeter service.
+//! It listens for incoming "Hello" requests and sends back a greeting message.
+
+// Import the auto-generated code for the "rpc_hello" module from the Proto file.
 pub mod rpc_hello {
     // The string specified here must match the proto package name
     mrpc::include_proto!("rpc_hello");
-    // include!("../../../mrpc/src/codegen.rs");
 }
 
 use rpc_hello::greeter_server::{Greeter, GreeterServer};
@@ -12,14 +15,19 @@ use mrpc::{RRef, WRef};
 #[derive(Debug, Default)]
 struct MyGreeter;
 
+// Implement the Greeter trait for MyGreeter using async_trait.
 #[mrpc::async_trait]
 impl Greeter for MyGreeter {
+    // Define the say_hello function which takes an RRef<HelloRequest>
+    // and returns a Result with a WRef<HelloReply>.
     async fn say_hello(
         &self,
         request: RRef<HelloRequest>,
     ) -> Result<WRef<HelloReply>, mrpc::Status> {
+        // Log the received request.
         eprintln!("request: {:?}", request);
 
+        // Create a new HelloReply with a greeting message.
         let message = format!("Hello {}!", String::from_utf8_lossy(&request.name));
         let reply = WRef::new(HelloReply {
             message: message.as_bytes().into(),
@@ -30,8 +38,11 @@ impl Greeter for MyGreeter {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start the server, binding it to port 5000.
     smol::block_on(async {
         let mut server = mrpc::stub::LocalServer::bind("0.0.0.0:5000")?;
+
+        // Add the Greeter service to the server using the custom MyGreeter implementation.
         server
             .add_service(GreeterServer::new(MyGreeter::default()))
             .serve()
