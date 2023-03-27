2da48f33fe14de5143840980c4e6c1ee61111da1
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-03-27 09:15:06 -0700

    fix fmt

--- experimental/mrpc/examples/rpc_echo/src/frontend.rs
+++ experimental/mrpc/examples/rpc_echo/src/frontend.rs
@@ -1,5 +1,5 @@
-//! This code is a simple Rust frontend application that listens for incoming connections, 
-//! send a RPC request to the echo server, and sends a response back to the client. 
+//! This code is a simple Rust frontend application that listens for incoming connections,
+//! send a RPC request to the echo server, and sends a response back to the client.
 
 pub mod rpc_echo {
     // The string specified here must match the proto package name
@@ -13,7 +13,6 @@ use std::{
     net::{TcpListener, TcpStream},
     thread,
 };
-
 
 // The main function that starts the server and listens for incoming connections.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -34,9 +33,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 // Function to handle the connection, read the request, and send the response.
-fn handle_connection(
-    mut stream: TcpStream,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let mut buf_reader = BufReader::new(&mut stream);
     let mut request_str = String::new();
     buf_reader.read_line(&mut request_str)?;
@@ -47,9 +44,7 @@ fn handle_connection(
 
     // Connect to the Greeter service and send the HelloRequest.
     let client = GreeterClient::connect("localhost:5000")?;
-    let req = HelloRequest {
-        name: uri.into(),
-    };
+    let req = HelloRequest { name: uri.into() };
     let reply = smol::block_on(client.say_hello(req))?;
     println!("reply: {}", String::from_utf8_lossy(&reply.message));
 
@@ -58,9 +53,7 @@ fn handle_connection(
     let content = String::from_utf8_lossy(&reply.message);
     let length = content.len();
 
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{content}"
-    );
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
 
     stream.write_all(response.as_bytes())?;
     Ok(())
