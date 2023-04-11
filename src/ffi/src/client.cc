363f839b4016cf7c4aab816771a9e460b2aefa34
nikolabo <nikolabo@live.com>
2023-04-11 01:13:25 -0700

    implement rpc requests asynchronously
    adds working runtime thread to client codegen

--- src/ffi/src/client.cc
+++ src/ffi/src/client.cc
@@ -1,21 +1,30 @@
-#include "ffi/src/codegen.rs"
+#include "ffi/src/clientcodegen.rs"
 #include <iostream>
 
 void sendRequest(IncrementerClient* client, ValueRequest* req) {
   std::cout << "request: ValueRequest { val: " << req->val() << " }" << std::endl;
-  ValueReply* reply = client->increment(rust::Box<ValueRequest>::from_raw(req)).into_raw();
+  client->increment(rust::Box<ValueRequest>::from_raw(req));
+}
+
+void receiveReply(rust::Box<ValueReply> reply) {
+  ValueReply* reply = reply.into_raw();
   std::cout << "response: ValueReply { val: " << reply->val() << " }" << std::endl;
 }
 
 int main() {
+  initialize();
   IncrementerClient* client_1 = connect("127.0.0.1:5000").into_raw();
-  ValueRequest* req = new_value_request().into_raw();
-  req->set_val(0);
-  for (int i = 0; i < 200; i++) {
-    req->add_foo(i);
-  }
 
-  sendRequest(client_1, req);
+  for (int i = 0; i < 5; i++) {
+    ValueRequest* req = new_value_request().into_raw();
+    req->set_val(i);
+    for (int i = 0; i < 10; i++) {
+      req->add_foo(i);
+    }
+    sendRequest(client_1, req);
+  }
   
+  while(true) {}
+
   return 0;
 }
--- src/ffi/src/clientcodegen.rs
+++ src/ffi/src/clientcodegen.rs
@@ -3,12 +3,28 @@
 
 #![no_main]
 
+use std::future::poll_fn;
 use std::sync::Arc;
+use std::task::Poll;
+use std::thread;
 
 use mrpc::{WRef};
 use mrpc::stub::{ClientStub, NamedService};
+use crossbeam_channel::{unbounded, bounded, Sender, Receiver};
+use lazy_static::lazy_static;
+use tokio::runtime::Builder;
+use tokio::task;
 
 include!("typescodegen.rs");
+
+lazy_static! {
+    static ref SEND_CHANNEL:(Sender<ClientWork>, Receiver<ClientWork>)  = {
+        unbounded()
+    };
+    static ref CONNECT_COMPLETE_CHANNEL:(Sender<usize>, Receiver<usize>)  = {
+        bounded(1)
+    };
+}
 
 #[cxx::bridge]
 mod incrementer_ffi {
@@ -16,9 +32,6 @@ mod incrementer_ffi {
         type ValueRequest;
         type ValueReply;
         type IncrementerClient;
-
-        fn connect(dst: String) -> Result<Box<IncrementerClient>>;
-        fn increment(self: &IncrementerClient, req: Box<ValueRequest>) ->  Result<Box<ValueReply>>;
 
         fn new_value_request() -> Box<ValueRequest>;
         fn val(self: &ValueRequest) -> u64;
@@ -31,6 +44,11 @@ mod incrementer_ffi {
         fn new_value_reply() -> Box<ValueReply>;
         fn val(self: &ValueReply) -> u64;
         fn set_val(self: &mut ValueReply, val: u64);
+
+        fn initialize();
+
+        fn connect(dst: String) -> Box<IncrementerClient>;
+        fn increment(self: &IncrementerClient, req: Box<ValueRequest>);
     }
 }
 
@@ -38,15 +56,72 @@ mod incrementer_ffi {
 
 #[derive(Debug)]
 pub struct IncrementerClient {
-    stub: Arc<ClientStub>,
+    client_handle: usize,
 }
 
-fn connect(dst: String) -> Result<Box<IncrementerClient>, ::mrpc::Error> {
+enum ClientWork {
+    Connect(String),
+    Increment(usize, Box<ValueRequest>),
+}
+
+fn initialize() {
+    println!("initializing mrpc stub...");
+    thread::spawn(|| {
+        println!("runtime thread started");
+        let runtime = Builder::new_current_thread()
+            .build()
+            .unwrap();
+        runtime.block_on(inside_runtime());
+    });
+}
+
+async fn inside_runtime() {
+    let local = task::LocalSet::new();
+
+    let mut clients: std::vec::Vec<Arc<ClientStub>> = std::vec::Vec::new();
+    println!("tokio current thread runtime starting...");
+
+    local.run_until(async move {
+        poll_fn(|cx| {
+            let v: Vec<ClientWork> = SEND_CHANNEL.1.try_iter().collect();   // TODO(nikolabo): client mapping stored in vector, handle is vector index, needs to be updated so clients can be deallocated
+
+            if v.len() > 0 { println!("runtime received something from channel") };
+
+            for i in v {
+                match i {
+                    ClientWork::Connect(dst) => {
+                        clients.push(connect_inner(dst));
+                        CONNECT_COMPLETE_CHANNEL.0.send(clients.len() - 1).unwrap();
+                        println!("runtime sent connect completion");
+                    },
+                    ClientWork::Increment(handle, req) => {
+                        println!("Increment request received by runtime thread");
+                        task::spawn_local(increment_inner(Arc::clone(&clients.get(handle).unwrap()), req));
+                    },
+                }
+            }
+
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        })
+        .await
+    }).await
+}
+
+fn connect(dst: String) -> Box<IncrementerClient> {
+    // TODO(nikolabo): connect panics on error
+    SEND_CHANNEL.0.send(ClientWork::Connect(dst)).unwrap();
+    Box::new(IncrementerClient { client_handle: CONNECT_COMPLETE_CHANNEL.1.recv().unwrap() })
+}
+
+fn connect_inner(dst: String) -> Arc<ClientStub> {
     // Force loading/reloading protos at the backend
-    update_protos()?;
+    println!("connection starting...");
+    update_protos().unwrap();
 
     let stub = ClientStub::connect(dst).unwrap();
-    Ok(Box::new(IncrementerClient { stub: Arc::clone(&stub) }))
+    println!("phoenix backend connection established");
+    Arc::clone(&stub)
 }
 
 fn update_protos() -> Result<(), ::mrpc::Error> {
@@ -61,31 +136,26 @@ impl IncrementerClient {
     fn increment(
         &self,
         req: Box<ValueRequest>,
-    ) ->  Result<Box<ValueReply>, ::mrpc::Status>
+    )
     {
-        let f = self.increment_inner(req);
-        let r = tokio::task::spawn_local(f);
-        // match r {
-        //     Ok(val) => Ok( Box::new(*val) ),
-        //     Err(error) => Err(error),
-        // }
-        Ok(new_value_reply())
+        SEND_CHANNEL.0.send(ClientWork::Increment(self.client_handle, req)).unwrap();
+        println!("Increment request sent to runtime thread...");
     }
+}
 
-    fn increment_inner(
-        &self,
-        req: Box<ValueRequest>,
-    ) -> impl std::future::Future<Output = Result<mrpc::RRef<ValueReply>, ::mrpc::Status>>
-    {
-        let call_id = self.stub.initiate_call();
-        // Fill this with the right func_id
-        let func_id = 3784353755;
+fn increment_inner(
+    stub: Arc<ClientStub>,
+    req: Box<ValueRequest>,
+) -> impl std::future::Future<Output = Result<mrpc::RRef<ValueReply>, ::mrpc::Status>>
+{
+    let call_id = stub.initiate_call();
+    // Fill this with the right func_id
+    let func_id = 3784353755;
 
-        let r = WRef::new(*req);    // TODO(nikolabo): Rust stub only writes RPC data once, directly to shm heap, we introduce an extra copy here, how to avoid?
+    let r = WRef::new(*req);    // TODO(nikolabo): Rust stub only writes RPC data once, directly to shm heap, we introduce an extra copy here, how to avoid?
 
-        self.stub
-            .unary(Self::SERVICE_ID, func_id, call_id, r)
-    }
+    stub
+        .unary(IncrementerClient::SERVICE_ID, func_id, call_id, r)
 }
 
 impl NamedService for IncrementerClient {
