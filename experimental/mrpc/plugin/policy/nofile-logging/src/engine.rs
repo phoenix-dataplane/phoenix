6518eae261936ff4a367b8198e938f3f08792966
lbr <lichangxinlivingshade@gmail.com>
2023-06-23 19:32:43 -0400

    add generated nofile-logging

---
+++ experimental/mrpc/plugin/policy/nofile-logging/src/engine.rs
@@ -0,0 +1,241 @@
+use anyhow::{anyhow, Result};
+use futures::future::BoxFuture;
+use std::fmt;
+use std::fs::File;
+use std::io::Write;
+use std::os::unix::ucred::UCred;
+use std::pin::Pin;
+
+use phoenix_api_policy_nofile_logging::control_plane;
+
+use phoenix_common::engine::datapath::message::{EngineRxMessage, EngineTxMessage};
+
+use phoenix_common::engine::datapath::node::DataPathNode;
+use phoenix_common::engine::{future, Decompose, Engine, EngineResult, Indicator, Vertex};
+use phoenix_common::envelop::ResourceDowncast;
+use phoenix_common::impl_vertex_for_engine;
+use phoenix_common::module::Version;
+use phoenix_common::storage::{ResourceCollection, SharedStorage};
+
+use super::DatapathError;
+use crate::config::{create_log_file, NofileLoggingConfig};
+
+use chrono::prelude::*;
+use phoenix_common::engine::datapath::RpcMessageTx;
+///use itertools::iproduct;
+
+pub struct struct_rpc_events_file {
+    pub timestamp: DateTime<Utc>,
+    pub event_type: String,
+    pub source: String,
+    pub destination: String,
+    pub rpc: String,
+}
+impl struct_rpc_events_file {
+    pub fn new(
+        timestamp: DateTime<Utc>,
+        event_type: String,
+        source: String,
+        destination: String,
+        rpc: String,
+    ) -> struct_rpc_events_file {
+        struct_rpc_events_file {
+            timestamp: timestamp,
+            event_type: event_type,
+            source: source,
+            destination: destination,
+            rpc: rpc,
+        }
+    }
+}
+impl fmt::Display for struct_rpc_events_file {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{},", self.timestamp);
+        write!(f, "{},", self.event_type);
+        write!(f, "{},", self.source);
+        write!(f, "{},", self.destination);
+        write!(f, "{},", self.rpc);
+        write!(f, "\n")
+    }
+}
+
+pub(crate) struct NofileLoggingEngine {
+    pub(crate) node: DataPathNode,
+    pub(crate) indicator: Indicator,
+    pub(crate) config: NofileLoggingConfig,
+    pub(crate) log_file: File,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Status {
+    Progress(usize),
+    Disconnected,
+}
+
+use Status::Progress;
+
+impl Engine for NofileLoggingEngine {
+    fn activate<'a>(self: Pin<&'a mut Self>) -> BoxFuture<'a, EngineResult> {
+        Box::pin(async move { self.get_mut().mainloop().await })
+    }
+
+    fn description(self: Pin<&Self>) -> String {
+        "NofileLoggingEngine".to_owned()
+    }
+
+    #[inline]
+    fn tracker(self: Pin<&mut Self>) -> &mut Indicator {
+        &mut self.get_mut().indicator
+    }
+
+    fn handle_request(&mut self, request: Vec<u8>, _cred: UCred) -> Result<()> {
+        let request: control_plane::Request = bincode::deserialize(&request[..])?;
+
+        match request {
+            control_plane::Request::NewConfig() => {
+                self.config = NofileLoggingConfig {};
+            }
+        }
+        Ok(())
+    }
+}
+
+impl_vertex_for_engine!(NofileLoggingEngine, node);
+
+impl Decompose for NofileLoggingEngine {
+    fn flush(&mut self) -> Result<usize> {
+        let mut work = 0;
+        while !self.tx_inputs()[0].is_empty() || !self.rx_inputs()[0].is_empty() {
+            if let Progress(n) = self.check_input_queue()? {
+                work += n;
+            }
+        }
+        Ok(work)
+    }
+
+    fn decompose(
+        self: Box<Self>,
+        _shared: &mut SharedStorage,
+        _global: &mut ResourceCollection,
+    ) -> (ResourceCollection, DataPathNode) {
+        let engine = *self;
+        let mut collections = ResourceCollection::with_capacity(4);
+        collections.insert("config".to_string(), Box::new(engine.config));
+        (collections, engine.node)
+    }
+}
+
+impl NofileLoggingEngine {
+    pub(crate) fn restore(
+        mut local: ResourceCollection,
+        node: DataPathNode,
+        _prev_version: Version,
+    ) -> Result<Self> {
+        let config = *local
+            .remove("config")
+            .unwrap()
+            .downcast::<NofileLoggingConfig>()
+            .map_err(|x| anyhow!("fail to downcast, type_name={:?}", x.type_name()))?;
+        let mut log_file = create_log_file();
+
+        let engine = NofileLoggingEngine {
+            node,
+            indicator: Default::default(),
+            config,
+            log_file,
+        };
+        Ok(engine)
+    }
+}
+
+impl NofileLoggingEngine {
+    async fn mainloop(&mut self) -> EngineResult {
+        loop {
+            let mut work = 0;
+            loop {
+                match self.check_input_queue()? {
+                    Progress(0) => break,
+                    Progress(n) => work += n,
+                    Status::Disconnected => return Ok(()),
+                }
+            }
+            self.indicator.set_nwork(work);
+            future::yield_now().await;
+        }
+    }
+}
+
+impl NofileLoggingEngine {
+    fn check_input_queue(&mut self) -> Result<Status, DatapathError> {
+        use phoenix_common::engine::datapath::TryRecvError;
+
+        match self.tx_inputs()[0].try_recv() {
+            Ok(msg) => {
+                match msg {
+                    EngineTxMessage::RpcMessage(msg) => {
+                        let meta_ref = unsafe { &*msg.meta_buf_ptr.as_meta_ptr() };
+                        // TODO! write to file
+                        let mut input = Vec::new();
+                        input.push(msg);
+                        for event in input
+                            .iter()
+                            .map(|req| {
+                                struct_rpc_events_file::new(
+                                    Utc::now(),
+                                    format!("{:?}", meta_ref.msg_type),
+                                    format!("{:?}", meta_ref.conn_id),
+                                    format!("{:?}", meta_ref.conn_id),
+                                    format!("{}", req.addr_backend.clone()),
+                                )
+                            })
+                            .collect::<Vec<_>>()
+                        {
+                            write!(self.log_file, "{}", event);
+                        }
+                        let output: Vec<_> = input
+                            .iter()
+                            .map(|req| {
+                                RpcMessageTx::new(
+                                    req.meta_buf_ptr.clone(),
+                                    req.addr_backend.clone(),
+                                )
+                            })
+                            .collect::<Vec<_>>();
+
+                        for msg in output {
+                            self.tx_outputs()[0].send(EngineTxMessage::RpcMessage(msg))?;
+                        }
+                    }
+                    m => self.tx_outputs()[0].send(m)?,
+                }
+                return Ok(Progress(1));
+            }
+            Err(TryRecvError::Empty) => {}
+            Err(TryRecvError::Disconnected) => {
+                return Ok(Status::Disconnected);
+            }
+        }
+
+        match self.rx_inputs()[0].try_recv() {
+            Ok(msg) => {
+                match msg {
+                    EngineRxMessage::Ack(rpc_id, status) => {
+                        // TODO! write to file
+                        // todo
+                        self.rx_outputs()[0].send(EngineRxMessage::Ack(rpc_id, status))?;
+                    }
+                    EngineRxMessage::RpcMessage(msg) => {
+                        self.rx_outputs()[0].send(EngineRxMessage::RpcMessage(msg))?;
+                    }
+                    m => self.rx_outputs()[0].send(m)?,
+                }
+                return Ok(Progress(1));
+            }
+            Err(TryRecvError::Empty) => {}
+            Err(TryRecvError::Disconnected) => {
+                return Ok(Status::Disconnected);
+            }
+        }
+        Ok(Progress(0))
+    }
+}
