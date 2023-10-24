1968ae997cd0edbaef43de3d7331ff1ebb9403dd
banruo <banruo@cs.washington.edu>
2023-10-24 03:59:48 -0400

    mutation

---
+++ experimental/mrpc/plugin/policy/mutation/src/engine.rs
@@ -0,0 +1,195 @@
+//! This engine can only be placed at the sender side for now.
+use std::num::NonZeroU32;
+use std::os::unix::ucred::UCred;
+use std::pin::Pin;
+use std::ptr::Unique;
+
+use anyhow::{anyhow, Result};
+use fnv::FnvHashMap as HashMap;
+use futures::future::BoxFuture;
+
+use phoenix_api::rpc::{RpcId, TransportStatus};
+use phoenix_api_policy_mutation::control_plane;
+
+use phoenix_common::engine::datapath::message::{EngineRxMessage, EngineTxMessage, RpcMessageTx};
+use phoenix_common::engine::datapath::node::DataPathNode;
+use phoenix_common::engine::{future, Decompose, Engine, EngineResult, Indicator, Vertex};
+use phoenix_common::envelop::ResourceDowncast;
+use phoenix_common::impl_vertex_for_engine;
+use phoenix_common::log;
+use phoenix_common::module::Version;
+use phoenix_common::storage::{ResourceCollection, SharedStorage};
+
+use super::DatapathError;
+use crate::config::MutationConfig;
+
+pub(crate) struct MutationEngine {
+    pub(crate) node: DataPathNode,
+
+    pub(crate) indicator: Indicator,
+
+    pub(crate) config: MutationConfig,
+
+    pub(crate) target: String,
+}
+
+pub mod hello {
+    // The string specified here must match the proto package name
+    include!("rpc_hello.rs");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Status {
+    Progress(usize),
+    Disconnected,
+}
+
+use Status::Progress;
+
+impl Engine for MutationEngine {
+    fn activate<'a>(self: Pin<&'a mut Self>) -> BoxFuture<'a, EngineResult> {
+        Box::pin(async move { self.get_mut().mainloop().await })
+    }
+
+    fn description(self: Pin<&Self>) -> String {
+        "MutationEngine".to_owned()
+    }
+
+    #[inline]
+    fn tracker(self: Pin<&mut Self>) -> &mut Indicator {
+        &mut self.get_mut().indicator
+    }
+
+    fn handle_request(&mut self, request: Vec<u8>, _cred: UCred) -> Result<()> {
+        let request: control_plane::Request = bincode::deserialize(&request[..])?;
+
+        match request {
+            control_plane::Request::NewConfig() => {
+                // Update config
+                self.config = MutationConfig {};
+            }
+        }
+        Ok(())
+    }
+}
+
+impl_vertex_for_engine!(MutationEngine, node);
+
+impl Decompose for MutationEngine {
+    fn flush(&mut self) -> Result<usize> {
+        let mut work = 0;
+        while !self.tx_inputs()[0].is_empty() || !self.rx_inputs()[0].is_empty() {
+            if let Progress(n) = self.check_input_queue()? {
+                work += n;
+            }
+        }
+        Ok(work)
+    }
+
+    fn decompose(
+        self: Box<Self>,
+        _shared: &mut SharedStorage,
+        _global: &mut ResourceCollection,
+    ) -> (ResourceCollection, DataPathNode) {
+        let engine = *self;
+
+        let mut collections = ResourceCollection::with_capacity(2);
+        collections.insert("target".to_string(), Box::new(engine.target));
+        collections.insert("config".to_string(), Box::new(engine.config));
+        (collections, engine.node)
+    }
+}
+
+impl MutationEngine {
+    pub(crate) fn restore(
+        mut local: ResourceCollection,
+        node: DataPathNode,
+        _prev_version: Version,
+    ) -> Result<Self> {
+        let config = *local
+            .remove("config")
+            .unwrap()
+            .downcast::<MutationConfig>()
+            .map_err(|x| anyhow!("fail to downcast, type_name={:?}", x.type_name()))?;
+        let target = *local
+            .remove("target")
+            .unwrap()
+            .downcast::<String>()
+            .map_err(|x| anyhow!("fail to downcast, type_name={:?}", x.type_name()))?;
+        let engine = MutationEngine {
+            node,
+            indicator: Default::default(),
+            target,
+            config,
+        };
+        Ok(engine)
+    }
+}
+
+impl MutationEngine {
+    async fn mainloop(&mut self) -> EngineResult {
+        loop {
+            let mut work = 0;
+            // check input queue, ~100ns
+            loop {
+                match self.check_input_queue()? {
+                    Progress(0) => break,
+                    Progress(n) => work += n,
+                    Status::Disconnected => return Ok(()),
+                }
+            }
+
+            // If there's pending receives, there will always be future work to do.
+            self.indicator.set_nwork(work);
+
+            future::yield_now().await;
+        }
+    }
+}
+
+/// Copy the RPC request to a private heap and returns the request.
+#[inline]
+fn materialize(msg: &RpcMessageTx) -> Box<hello::HelloRequest> {
+    let req_ptr = Unique::new(msg.addr_backend as *mut hello::HelloRequest).unwrap();
+    let req = unsafe { req_ptr.as_ref() };
+    // returns a private_req
+    Box::new(req.clone())
+}
+
+impl MutationEngine {
+    fn check_input_queue(&mut self) -> Result<Status, DatapathError> {
+        use phoenix_common::engine::datapath::TryRecvError;
+
+        match self.tx_inputs()[0].try_recv() {
+            Ok(msg) => {
+                match msg {
+                    EngineTxMessage::RpcMessage(msg) => {
+                        let mut req_ptr =
+                            Unique::new(msg.addr_backend as *mut hello::HelloRequest).unwrap();
+                        let req = unsafe { req_ptr.as_mut() };
+                        for i in 0..req.name.len() {
+                            req.name[i] = 'a' as u8;
+                        }
+                        self.tx_outputs()[0].send(EngineTxMessage::RpcMessage(msg))?;
+                    }
+                    m => self.tx_outputs()[0].send(m)?,
+                }
+                return Ok(Progress(1));
+            }
+            Err(TryRecvError::Empty) => {}
+            Err(TryRecvError::Disconnected) => return Ok(Status::Disconnected),
+        }
+
+        // forward all rx msgs
+        match self.rx_inputs()[0].try_recv() {
+            Ok(m) => {
+                self.rx_outputs()[0].send(m)?;
+                return Ok(Progress(1));
+            }
+            Err(TryRecvError::Empty) => {}
+            Err(TryRecvError::Disconnected) => return Ok(Status::Disconnected),
+        }
+
+        Ok(Progress(0))
+    }
+}
