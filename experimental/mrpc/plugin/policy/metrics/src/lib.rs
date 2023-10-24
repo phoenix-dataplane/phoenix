536918e0e5fa2ccf64b56820dc9b16b1c84bd390
banruo <banruo@cs.washington.edu>
2023-10-23 22:11:04 -0400

    metrics

---
+++ experimental/mrpc/plugin/policy/metrics/src/lib.rs
@@ -0,0 +1,34 @@
+#![feature(peer_credentials_unix_socket)]
+#![feature(ptr_internals)]
+#![feature(strict_provenance)]
+
+use thiserror::Error;
+
+pub use phoenix_common::{InitFnResult, PhoenixAddon};
+
+pub mod config;
+pub(crate) mod engine;
+pub mod module;
+
+#[derive(Error, Debug)]
+pub(crate) enum DatapathError {
+    #[error("Internal queue send error")]
+    InternalQueueSend,
+}
+
+use phoenix_common::engine::datapath::SendError;
+impl<T> From<SendError<T>> for DatapathError {
+    fn from(_other: SendError<T>) -> Self {
+        DatapathError::InternalQueueSend
+    }
+}
+
+use crate::config::MetricsConfig;
+use crate::module::MetricsAddon;
+
+#[no_mangle]
+pub fn init_addon(config_string: Option<&str>) -> InitFnResult<Box<dyn PhoenixAddon>> {
+    let config = MetricsConfig::new(config_string)?;
+    let addon = MetricsAddon::new(config);
+    Ok(Box::new(addon))
+}
